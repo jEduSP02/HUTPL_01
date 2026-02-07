87d603e30c7e86239714386f977d86c29eb1e919
jEduSP02 <jesanchez44@utpl.edu.ec>
2026-02-07 16:02:27 -0500

    Update wsgi.py
    Creando superusuario temporal

--- herbario/wsgi.py
+++ herbario/wsgi.py
@@ -1,5 +1,13 @@
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herbario.settings')
 application = get_wsgi_application()
+
+User = get_user_model()
+if os.getenv("CREATE_SUPERUSER") == "1":
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@outlook.com", "01234")
+        print("Superusuario creado automáticamente.")
