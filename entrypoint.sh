14fc97951b42673336ac63b7c09ec2905ba9e4ef
jEduSP02 <jesanchez44@utpl.edu.ec>
2026-02-07 17:04:46 -0500

    Create entrypoint.sh
    Entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+echo "==> Esperando variables requeridas..."
+: "${PORT:?PORT no está definido}"
+# Si usas DB en Render:
+# : "${DATABASE_URL:?DATABASE_URL no está definido}"
+
+echo "==> Ejecutando migraciones..."
+python manage.py migrate --noinput
+
+echo "==> Recolectando estáticos..."
+python manage.py collectstatic --noinput
+
+echo "==> Iniciando Gunicorn en el puerto $PORT..."
+exec gunicorn herbario.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
