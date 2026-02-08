db0488e471d01ba3357de695e5d6aecd0cbe43f1
jEduSP02 <jesanchez44@utpl.edu.ec>
2026-02-07 20:37:02 -0500

    Create admin.py

---
+++ herbario/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Herbario UTL – Administración"
+admin.site.site_title = "Panel de Control HUTPL"
+admin.site.index_title = "Bienvenido al Sistema de Administración del Herbario"
