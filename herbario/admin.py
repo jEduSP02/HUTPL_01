dd1ef9a334e07db7b48e0cb1df40f6128a725e0f
jEduSP02 <jesanchez44@utpl.edu.ec>
2026-02-07 20:46:57 -0500

    Delete herbario/admin.py

--- herbario/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "Herbario UTL – Administración"
-admin.site.site_title = "Panel de Control HUTPL"
-admin.site.index_title = "Bienvenido al Sistema de Administración del Herbario"
